0ce1d5c1dce7a85c1516cbd960f1cef962666d50
Kflix <darayoeurn@gmail.com>
2025-10-04 09:02:46 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,33 @@
+import { NextPage } from "next";
 import dynamic from "next/dynamic";
+import Script from "next/script";
+
 const ContinueWatching = dynamic(() => import("@/components/sections/Home/ContinueWatching"));
 const HomePageList = dynamic(() => import("@/components/sections/Home/List"));
-const CustomIframeAd = dynamic(() => import("@/components/ads/CustomIframeAd"));
 
-const HomePage = () => {
+const HomePage: NextPage = () => {
   return (
     <div className="flex flex-col gap-3 md:gap-8">
-      <ContinueWatching />  
-       <HomePageList />
-      {/* Ad placement: Between sections */}
-      <div className="my-4 md:my-8 text-center">
-        <CustomIframeAd
-          adKey="297ed86b29d181f660479ebab2f97717"
-          width={300}
-          height={250}
-          format="iframe"
+      <ContinueWatching />
+      <div className="w-full max-w-[300px] mx-auto h-[250px]">
+        <div id="ad-container-297ed86b29d181f660479ebab2f97717"></div>
+        <Script id="ad-script" strategy="afterInteractive">
+          {`
+            atOptions = {
+              'key': '297ed86b29d181f660479ebab2f97717',
+              'format': 'iframe',
+              'height': 250,
+              'width': 300,
+              'params': {}
+            };
+          `}
+        </Script>
+        <Script
+          src="//www.highperformanceformat.com/297ed86b29d181f660479ebab2f97717/invoke.js"
+          strategy="afterInteractive"
         />
-      </div>  
+      </div>
+      <HomePageList />
     </div>
   );
 };
