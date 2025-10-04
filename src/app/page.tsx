c711e51d9f289b94ca52957781d6b8ff1272df2d
Kflix <darayoeurn@gmail.com>
2025-10-04 09:09:28 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,23 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
-import Script from "next/script";
-
 const ContinueWatching = dynamic(() => import("@/components/sections/Home/ContinueWatching"));
 const HomePageList = dynamic(() => import("@/components/sections/Home/List"));
+import BannerAd from "@/components/ads/BannerAd";
 
 const HomePage: NextPage = () => {
   return (
     <div className="flex flex-col gap-3 md:gap-8">
       <ContinueWatching />
-      <div className="w-full max-w-[300px] mx-auto h-[250px]">
-        <div id="ad-container-297ed86b29d181f660479ebab2f97717"></div>
-        <Script id="ad-script" strategy="afterInteractive">
-          {`
-            atOptions = {
-              'key': '297ed86b29d181f660479ebab2f97717',
-              'format': 'iframe',
-              'height': 250,
-              'width': 300,
-              'params': {}
-            };
-          `}
-        </Script>
-        <Script
-          src="//www.highperformanceformat.com/297ed86b29d181f660479ebab2f97717/invoke.js"
-          strategy="afterInteractive"
+      <HomePageList />
+            {/* ✅ Banner Ad at the bottom */}
+      <div className="flex justify-center my-8">
+        <BannerAd
+          adKey="297ed86b29d181f660479ebab2f97717"
+          width={300}
+          height={250}
+          id="ad-bottom"
         />
       </div>
-      <HomePageList />
     </div>
   );
 };
