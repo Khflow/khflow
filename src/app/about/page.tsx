f26e2d0a81063bbc371334552542f321c57e55a1
Kflix <darayoeurn@gmail.com>
2025-10-07 14:17:48 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ import { NextPage } from "next";
 const FAQ = dynamic(() => import("@/components/sections/About/FAQ"));
 
 export const metadata: Metadata = {
-  title: `About | ${siteConfig.name}`,
+  title: `About`,
 };
 
 const AboutPage: NextPage = () => {
