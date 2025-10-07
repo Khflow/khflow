4dcbe5fec01c7507bc14164caba104802b2c638a
Kflix <darayoeurn@gmail.com>
2025-10-07 13:46:37 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ import { NextPage } from "next";
 const FAQ = dynamic(() => import("@/components/sections/About/FAQ"));
 
 export const metadata: Metadata = {
-  title: "Discover Movies",
+  title: "About",
 };
 
 const AboutPage: NextPage = () => {
